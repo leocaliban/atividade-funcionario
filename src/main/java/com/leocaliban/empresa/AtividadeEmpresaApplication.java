303a7404c2d4c4e63d89528db081c9d8a10f3100
Leocaliban <leocaliban@gmail.com>
2018-01-04 20:19:58 -0300

    Inicia projeto

---
+++ src/main/java/com/leocaliban/empresa/AtividadeEmpresaApplication.java
@@ -0,0 +1,12 @@
+package com.leocaliban.empresa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadeEmpresaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtividadeEmpresaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/leocaliban/empresa/AtividadeEmpresaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.leocaliban.empresa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AtividadeEmpresaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
