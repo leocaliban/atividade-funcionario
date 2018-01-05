1ef4b00baa6878d25a2584d40907ab8653ff6251
Leocaliban <leocaliban@gmail.com>
2018-01-05 03:28:02 -0300

    Aplica mapeamento JPA

--- src/main/java/com/leocaliban/empresa/domain/Funcionario.java
+++ src/main/java/com/leocaliban/empresa/domain/Funcionario.java
@@ -3,11 +3,20 @@ package com.leocaliban.empresa.domain;
 import java.io.Serializable;
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Funcionario implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	private String nome;
 	private String cpf;
 	private Date dataNascimento;
